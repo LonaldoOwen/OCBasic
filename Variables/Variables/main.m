4962bdba32db3de7874eaf7b3dc9f99161a7cd84
libowen <libowen@cct.cn>
2018-03-26 15:52:02 +0800

    init git

---
+++ Variables/Variables/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Variables
+//
+//  Created by libowen on 2018/3/26.
+//  Copyright © 2018年 libowen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
